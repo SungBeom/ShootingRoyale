d682403fcd8d4b4a7238bc66c5eefc79f47b27c0
SungBeom <psb_94@naver.com>
2018-12-23 19:03:01 +0900

    Make Network Manager

---
+++ ShootingRoyale/Assets/Scripts/Health.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+
+public class Health : NetworkBehaviour {
+
+    public const int maxHealth = 100;
+
+    [SyncVar(hook = "OnChangeHealth")]
+    public int currentHealth = maxHealth;
+
+    public Slider healthSlider;
+
+    public void TakeDamage(int amount)
+    {
+        if (!isServer)
+        {
+            return;
+        }
+
+        currentHealth -= amount;
+    }
+
+    void OnChangeHealth(int health)
+    {
+        healthSlider.value = health;
+    }
+}
