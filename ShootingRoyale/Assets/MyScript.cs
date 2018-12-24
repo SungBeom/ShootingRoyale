c6329b082b8028d7bb137faa2a4630e38d076910
SungBeom <psb_94@naver.com>
2018-12-25 03:00:25 +0900

    Move Throgh Joystick

---
+++ ShootingRoyale/Assets/MyScript.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyScript : MonoBehaviour
+{
+
+    protected Joystick joystick;
+    protected Joybutton joybutton;
+
+    protected bool jump;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        joystick = FindObjectOfType<Joystick>();
+        joybutton = FindObjectOfType<Joybutton>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        var rigidbody = GetComponent<Rigidbody>();
+
+        rigidbody.velocity = new Vector3(joystick.Horizontal * 100f + Input.GetAxis("Horizontal"),
+            rigidbody.velocity.y,
+            joystick.Vertical * 100f + Input.GetAxis("Vertical"));
+
+        if (!jump && (joybutton.Pressed || Input.GetButtonDown("Fire2")))
+        {
+            jump = true;
+            rigidbody.velocity += Vector3.up * 20f;
+        }
+
+        if (jump && !joybutton.Pressed || Input.GetButtonDown("Fire2"))
+        {
+            jump = false;
+        }
+    }
+}
