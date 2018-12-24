ca24854000e90e1a0438f8fdf08d3d48cc0f4a6c
SungBeom <psb_94@naver.com>
2018-12-24 16:38:26 +0900

    Update Move

---
+++ ShootingRoyale/Assets/Scripts/Move.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class Move : MonoBehaviour,IDragHandler,IPointerUpHandler,IPointerDownHandler {
+
+    Vector3 FirstPos;   // 첫 번째 누른곳
+    Vector3 MovePos;        // 이동 된 곳
+    private Vector3 Vec;
+    public GameObject Player;
+    public float MoveSpeed = 5.0f;
+
+    void Update()
+    {
+        if (Input.GetTouch(0).position.x < Screen.width / 2)
+        {
+            if (Input.GetTouch(0).phase == TouchPhase.Began)
+                FirstPos = Input.GetTouch(0).position;
+            if (Input.GetTouch(0).phase == TouchPhase.Moved)
+            {
+                MovePos = Input.GetTouch(0).position;
+                Vec = (MovePos - FirstPos).normalized;
+                Player.transform.Translate(new Vector3(-Vec.y, 0, Vec.x) * Time.deltaTime * MoveSpeed);
+            }
+        }
+        else
+        {
+            if (Input.GetTouch(0).phase == TouchPhase.Began)
+                FirstPos = Input.GetTouch(0).position;
+            if(Input.GetTouch(0).phase == TouchPhase.Moved)
+            {
+                MovePos = Input.GetTouch(0).position;
+                Vec = (MovePos - FirstPos).normalized;
+                Player.transform.Rotate(0,Vec.x,0);
+            }
+        }
+    }
+
+    public virtual void OnPointerDown(PointerEventData ped)
+    {
+
+    }
+
+    public virtual void OnPointerUp(PointerEventData ped)
+    {
+
+    }
+
+    public virtual void OnDrag(PointerEventData ped)
+    {
+
+    }
+}
--- ShootingRoyale/Assets/Scripts/PlayerController.cs
+++ ShootingRoyale/Assets/Scripts/PlayerController.cs
@@ -5,25 +5,58 @@ using UnityEngine.Networking;
 
 public class PlayerController : NetworkBehaviour {
 
+    Vector2 firstPos;
+    Vector2 movePos;
+    Vector2 direction;
+
+    public float moveSpeed;
+
     public GameObject bulletPrefab;
 
     public Transform bulletSpawn;
-	
-	// Update is called once per frame
-	void Update () {
+
+    // Update is called once per frame
+    void Update() {
 
         if (!isLocalPlayer)
         {
             return;
         }
 
-        var x = Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
-        var z = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
+        // 이동 부분(수정)
+        //var x = Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
+        //var z = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
 
-        transform.Rotate(0, x, 0);
-        transform.Translate(0, 0, z);
+        //transform.Rotate(0, x, 0);
+        //transform.Translate(0, 0, z);
 
-        if(Input.GetKeyDown(KeyCode.Space))
+        for (int i = 0; i < Input.touchCount; i++)
+        {
+            if (TouchSide(Input.GetTouch(i).position))
+            {
+                if (Input.GetTouch(i).phase == TouchPhase.Began)
+                    firstPos = Input.GetTouch(i).position;
+                if (Input.GetTouch(i).phase == TouchPhase.Moved)
+                {
+                    movePos = Input.GetTouch(i).position;
+                    direction = (movePos - firstPos).normalized;
+                    transform.Translate(new Vector3(-direction.y, 0, direction.x) * Time.deltaTime * moveSpeed);
+                }
+            }
+            else
+            {
+                if (Input.GetTouch(i).phase == TouchPhase.Began)
+                    firstPos = Input.GetTouch(i).position;
+                if (Input.GetTouch(i).phase == TouchPhase.Moved)
+                {
+                    movePos = Input.GetTouch(i).position;
+                    direction = (movePos - firstPos).normalized;
+                    transform.Rotate(0, direction.x, 0);
+                }
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
         {
             CmdFire();
         }
@@ -39,6 +72,14 @@ public class PlayerController : NetworkBehaviour {
         NetworkServer.Spawn(bullet);
 
         Destroy(bullet, 2.0f);
+    }
+
+    bool TouchSide(Vector2 touchPos)
+    {
+        if (touchPos.x < Screen.width / 2)
+            return true;
+        else
+            return false;
     }
 
     public override void OnStartLocalPlayer()
