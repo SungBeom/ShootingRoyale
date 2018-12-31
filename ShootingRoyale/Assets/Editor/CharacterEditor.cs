ed5d4a532962f489cb9287b4c63c9cb4e33a4571
SungBeom <psb_94@naver.com>
2019-01-01 00:59:00 +0900

    Change Guns

---
+++ ShootingRoyale/Assets/Editor/CharacterEditor.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor (typeof (ChangeGun))]
+public class CharacterEditor : Editor {
+
+    public override void OnInspectorGUI()
+    {
+        ChangeGun gun = target as ChangeGun;
+
+        if (DrawDefaultInspector())
+        {
+            gun.Change();
+        }
+
+        if (GUILayout.Button("Change Gun"))
+        {
+            gun.Change();
+        }
+    }
+}
---
+++ ShootingRoyale/Assets/Scripts/ChangeGun.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeGun : MonoBehaviour
+{
+    public GameObject[] guns;
+    [Range(0, 3)]               // 4 대신 (guns.Length - 1)을 넣는 것이 합당함
+    public int selected;
+
+    GameObject gun;
+    int temp;
+
+    public void Change()
+    {
+        guns[temp].SetActive(false);
+        guns[selected].SetActive(true);
+        temp = selected;
+    }
+}
