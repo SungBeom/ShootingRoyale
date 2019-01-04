dadb6d6ec9ae177d37b4c883532fec3b1e2abf44
deious <gkstjddlekd@naver.com>
2019-01-04 23:09:45 +0900

    Change UI

---
+++ ShootingRoyale/Assets/Scripts/SceneChange.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChange : MonoBehaviour
+{
+    public void Scenechange()
+    {
+        SceneManager.LoadScene("ShootingRoyale");
+    }
+    public void Exit()
+    {
+        Application.Quit();
+    }
+}
