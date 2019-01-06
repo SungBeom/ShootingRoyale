10a829b7c0bdaf225bc831b8117020bf9bb7d5f1
deious <gkstjddlekd@naver.com>
2019-01-06 23:16:20 +0900

    Use Item Test

--- ShootingRoyale/Assets/Scripts/ChangeGun.cs
+++ ShootingRoyale/Assets/Scripts/ChangeGun.cs
@@ -11,7 +11,7 @@ public class ChangeGun : MonoBehaviour
     GameObject gun;
     int temp;
 
-    public void Change()
+    public void Change()        // 수류탄과 연막탄은 어차피 던질때 바꿔 들어야 하는데 여기에 쓰는게 낫지않을까?
     {
         guns[temp].SetActive(false);
         guns[selected].SetActive(true);
---
+++ ShootingRoyale/Assets/Scripts/UseExpendable.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UseExpendable : MonoBehaviour
+{
+    int count = 1;
+    public Button btn;
+    public GameObject go;
+
+    void Start()
+    {
+        GetComponent<Text>().text = "" + count;
+    }
+
+    void Update()
+    {
+        if (count > 0)
+        {
+            btn.interactable = true;
+        }
+        else
+        {
+            btn.interactable = false;
+        }
+    }
+
+    public void UsePortion()
+    {
+        count--;
+        GetComponent<Text>().text = "" + count;
+        if (go.GetComponent<Slider>().value > 30)
+        {
+            go.GetComponent<Slider>().value = 100;
+        }
+        else
+        {
+            go.GetComponent<Slider>().value += 70;
+        }
+    }
+
+    /*public void UseAdrenaline()
+    {
+        GetComponent<Text>
+    }*/
+
+    /*public void ThrowGrenade()
+    {
+
+    }*/
+}
