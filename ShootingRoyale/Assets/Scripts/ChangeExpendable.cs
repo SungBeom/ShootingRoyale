1349c65ee5fcf171501a205e5c7146c5b0c56bf2
SungBeom <psb_94@naver.com>
2019-01-07 21:51:59 +0900

    Get and Use Expendables

---
+++ ShootingRoyale/Assets/Scripts/ChangeExpendable.cs
@@ -0,0 +1,131 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChangeExpendable : MonoBehaviour
+{
+    // enum State { GET, USE }
+    //int count = 1;
+    //public Button btn;
+    //public GameObject go;
+    public delegate void UseFunction();
+    UseFunction[] useFunction;
+
+    void Start()
+    {
+        // GetComponent<Text>().text = "" + count;
+        transform.GetChild(0).GetComponent<Text>().text = "3";
+        useFunction = new UseFunction[]
+        {
+            new UseFunction(UsePotion),
+            new UseFunction(UseAdrenaline),
+            new UseFunction(ThrowGrenade)
+        };
+    }
+
+    void Update()
+    {
+        //if (count > 0)
+        //{
+        //    btn.interactable = true;
+        //}
+        //else
+        //{
+        //    btn.interactable = false;
+        //}
+    }
+
+    public void GetExpendable()
+    {
+        int count;
+        Text text = transform.GetChild(0).GetComponent<Text>();
+
+        if (int.TryParse(text.text, out count))
+        {
+            text.text = (count + 1).ToString();
+            gameObject.GetComponent<Button>().interactable = true;
+        }
+    }
+
+    public void UseExpendable(int index)
+    {
+        int count;
+        Text text = transform.GetChild(0).GetComponent<Text>();
+
+        if (int.TryParse(text.text, out count))
+        {
+            useFunction[index]();
+
+            text.text = (count - 1).ToString();
+            if (count - 1 == 0)
+                gameObject.GetComponent<Button>().interactable = false;
+        }
+    }
+
+    //public void ChangeExpendableState(int index, int state)
+    //{
+    //    int count;
+    //    Text text = transform.GetChild(0).GetComponent<Text>();
+
+    //    if (state == 0)
+    //    {
+    //        if (int.TryParse(text.text, out count))
+    //        {
+    //            text.text = (count + 1).ToString();
+    //            gameObject.GetComponent<Button>().interactable = true;
+    //        }
+    //    }
+
+    //    else if (state == 1)
+    //    {
+    //        if (int.TryParse(transform.GetChild(0).GetComponent<Text>().text, out count))
+    //        {
+    //            // 각 소모품(index)에 맞는 사용 효과를 넣기
+
+    //            text.text = (count - 1).ToString();
+    //            if (count - 1 == 0)
+    //                gameObject.GetComponent<Button>().interactable = false;
+    //        }
+    //    }
+    //}
+
+    void UsePotion()
+    {
+
+    }
+
+    void UseAdrenaline()
+    {
+
+    }
+
+    void ThrowGrenade()
+    {
+
+    }
+
+    /*public void UsePotion()
+    {
+        count--;
+        GetComponent<Text>().text = "" + count;
+        if (go.GetComponent<Slider>().value > 30)
+        {
+            go.GetComponent<Slider>().value = 100;
+        }
+        else
+        {
+            go.GetComponent<Slider>().value += 70;
+        }
+    }*/
+
+    /*public void UseAdrenaline()
+    {
+        GetComponent<Text>
+    }*/
+
+    /*public void ThrowGrenade()
+    {
+
+    }*/
+}
--- ShootingRoyale/Assets/Scripts/MoveControl.cs
+++ ShootingRoyale/Assets/Scripts/MoveControl.cs
@@ -1,14 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityStandardAssets.Characters.FirstPerson;
 
 public class MoveControl : MonoBehaviour
 {
-
     public FixedJoystick MoveJoystick;
     public FixedButton JumpButton;
     public FixedTouchField TouchField;
+
+    public Button[] ExpendableBtn;
 
     void Update()
     {
@@ -17,5 +19,20 @@ public class MoveControl : MonoBehaviour
         fps.RunAxis = MoveJoystick.Direction;
         fps.JumpAxis = JumpButton.Pressed;
         fps.mouseLook.LookAxis = TouchField.TouchDist;
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        // 소모품이 각각 얻어질 시 해당 소모품에 맞는 태그로 비교
+        // 만약 소모품 중 랜덤하게 얻어진다면 소모품 태그로 변경 후,
+        // 내부에서 Btn 배열의 크기에 맞는 index 값을 랜덤하게 추출 -> 해당하는 소모품의 수를 늘려줌
+        // (근데 소모품을 얻고난 뒤에 버튼 UI를 기억과 비교해서 무엇을 얻었는지 보는 것이 맞는가...?)
+        // (해당 방식을 유지하기 위해서는 알림창을 띄워주는 것이 합당 -> 전투 중에는 방해일 수도 있음)
+        // (상자를 부숴서 얻는 방식도 고려 -> 총알의 소모가 있다는 시스템적인 변화가 생김)
+        if (col.gameObject.tag.Equals("Potion"))
+        {
+            ExpendableBtn[0].GetComponent<ChangeExpendable>().GetExpendable();
+            Destroy(col.gameObject);
+        }
     }
 }
