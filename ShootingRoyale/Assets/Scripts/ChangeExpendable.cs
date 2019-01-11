a7b11e9f1bb3d10c8900a602d257eda611fde742
SungBeom <psb_94@naver.com>
2019-01-11 12:57:24 +0900

    Add Effects of Potions

--- ShootingRoyale/Assets/Scripts/ChangeExpendable.cs
+++ ShootingRoyale/Assets/Scripts/ChangeExpendable.cs
@@ -5,36 +5,24 @@ using UnityEngine.UI;
 
 public class ChangeExpendable : MonoBehaviour
 {
-    // enum State { GET, USE }
-    //int count = 1;
-    //public Button btn;
-    //public GameObject go;
+    ExpendableControl expendableControl;
+
     delegate void UseFunction();
     UseFunction[] useFunction;
 
     void Start()
     {
-        // GetComponent<Text>().text = "" + count;
+        // 테스트를 위해 임의로 초기화
         transform.GetChild(0).GetComponent<Text>().text = "3";
+
+        expendableControl = transform.parent.GetComponent<ExpendableControl>();
 
         useFunction = new UseFunction[]
         {
-            new UseFunction(UseFirstAidKit),
-            new UseFunction(UseAdrenaline),
-            new UseFunction(UseSmokeShell)
+            new UseFunction(expendableControl.UseFirstAidKit),
+            new UseFunction(expendableControl.UseAdrenaline),
+            new UseFunction(expendableControl.UseSmokeShell)
         };
-    }
-
-    void Update()
-    {
-        //if (count > 0)
-        //{
-        //    btn.interactable = true;
-        //}
-        //else
-        //{
-        //    btn.interactable = false;
-        //}
     }
 
     public void GetExpendable()
@@ -63,70 +51,4 @@ public class ChangeExpendable : MonoBehaviour
                 gameObject.GetComponent<Button>().interactable = false;
         }
     }
-
-    //public void ChangeExpendableState(int index, int state)
-    //{
-    //    int count;
-    //    Text text = transform.GetChild(0).GetComponent<Text>();
-
-    //    if (state == 0)
-    //    {
-    //        if (int.TryParse(text.text, out count))
-    //        {
-    //            text.text = (count + 1).ToString();
-    //            gameObject.GetComponent<Button>().interactable = true;
-    //        }
-    //    }
-
-    //    else if (state == 1)
-    //    {
-    //        if (int.TryParse(transform.GetChild(0).GetComponent<Text>().text, out count))
-    //        {
-    //            // 각 소모품(index)에 맞는 사용 효과를 넣기
-
-    //            text.text = (count - 1).ToString();
-    //            if (count - 1 == 0)
-    //                gameObject.GetComponent<Button>().interactable = false;
-    //        }
-    //    }
-    //}
-
-    void UseFirstAidKit()
-    {
-
-    }
-
-    void UseAdrenaline()
-    {
-
-    }
-
-    void UseSmokeShell()
-    {
-
-    }
-
-    /*public void UsePotion()
-    {
-        count--;
-        GetComponent<Text>().text = "" + count;
-        if (go.GetComponent<Slider>().value > 30)
-        {
-            go.GetComponent<Slider>().value = 100;
-        }
-        else
-        {
-            go.GetComponent<Slider>().value += 70;
-        }
-    }*/
-
-    /*public void UseAdrenaline()
-    {
-        GetComponent<Text>
-    }*/
-
-    /*public void ThrowGrenade()
-    {
-
-    }*/
 }
---
+++ ShootingRoyale/Assets/Scripts/ExpendableControl.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityStandardAssets.Characters.FirstPerson;
+
+public class ExpendableControl : MonoBehaviour
+{
+    public Slider health;
+    // 현재 사람의 속도를 증가시키나 총 속도를 증가시키는 쪽으로 변경 예정
+    public RigidbodyFirstPersonController controller;
+
+    struct MovementSettings
+    {
+        public float ForwardSpeed;
+        public float BackwardSpeed;
+        public float StrafeSpeed;
+        public float JumpForce;
+    }
+    MovementSettings movementSettings;
+
+    [Range(0, 100)]
+    public float healingAmount;
+    public float accelerationTime;
+    [Range(1, 10)]
+    public float accelerationRate;
+
+    public void UseFirstAidKit()
+    {
+        Debug.Log(health.maxValue);
+        Debug.Log("UseFirstAidKit - " + healingAmount + " heal");
+
+        if (health.value >= health.maxValue - healingAmount) health.value = health.maxValue;
+        else health.value += healingAmount;
+    }
+
+    public void UseAdrenaline()
+    {
+        Debug.Log("UseAdrenaline - (" + accelerationTime + ", " + accelerationRate + ")");
+
+        movementSettings.ForwardSpeed = controller.movementSettings.ForwardSpeed;
+        movementSettings.BackwardSpeed = controller.movementSettings.BackwardSpeed;
+        movementSettings.StrafeSpeed = controller.movementSettings.StrafeSpeed;
+        movementSettings.JumpForce = controller.movementSettings.JumpForce;
+
+        controller.movementSettings.ForwardSpeed *= accelerationRate;
+        controller.movementSettings.BackwardSpeed *= accelerationRate;
+        controller.movementSettings.StrafeSpeed *= accelerationRate;
+        controller.movementSettings.JumpForce *= accelerationRate;
+
+        Invoke("CalmDown", accelerationTime);
+    }
+
+    public void UseSmokeShell()
+    {
+
+    }
+
+    void CalmDown()
+    {
+        Debug.Log("CalmDown...");
+
+        controller.movementSettings.ForwardSpeed = movementSettings.ForwardSpeed;
+        controller.movementSettings.BackwardSpeed = movementSettings.BackwardSpeed;
+        controller.movementSettings.StrafeSpeed = movementSettings.StrafeSpeed;
+        controller.movementSettings.JumpForce = movementSettings.JumpForce;
+    }
+}
